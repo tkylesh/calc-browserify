44ad1ed59731d2deedca9419eda545bc4a1dbaa1
Taylor Shuff <tkylesh@gmail.com>
2016-10-11 14:27:59 -0500

    added the rest of the calc-browserify app

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let MyCalculator = require("./calculator");
+
+console.log("add 9 10", MyCalculator.add(9, 10));
+console.log("subtract 10 9", MyCalculator.subtract(10, 9));
+
+console.log("MyCalculator", MyCalculator);
---
+++ javascripts/multiply.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function multiply (one, two) {
+  return one * two;
+}
+
+module.exports = multiply;
